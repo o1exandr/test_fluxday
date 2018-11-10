80b8b20022fea80a7ae5ce8549bdf08c17f574d2
Oleksandr Finiuk <olexandr.finyuk@gmail.com>
2018-11-10 21:52:50 +0200

    add NUnit, Selenium; run test loginpage

---
+++ test_fluxday/PageTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace test_fluxday
+{
+    public class PageTest
+    {
+        [Test]
+        public void site_login_page()
+        {
+            
+            IWebDriver browser = new FirefoxDriver();
+            browser.Navigate().GoToUrl("https://app.fluxday.io/");
+            //IWebElement header = browser.FindElement(By.Id("Fluxday"));
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            //Assert.True(header.Displayed);
+            browser.Close();
+
+        }
+    }
+}
